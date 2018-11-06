2c88a93a334e58517e75b7656e5d523c3027b668
gberg2000 <44782529+gberg2000@users.noreply.github.com>
2018-11-06 10:43:44 -0800

    Add files via upload

---
+++ sprites.py
@@ -0,0 +1,54 @@
+# sprite classes for game
+
+import pygame as pg
+from pygame.sprite import Sprite
+import random
+from settings import *
+
+class Player(Sprite):
+    def __init__(self):
+        Sprite.__init__(self)
+        self.image = pg.Surface((80,100))
+        self.image.fill(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH / 2, HEIGHT /2)
+        self.vx = 0
+        self.vy = 0
+    def update(self):
+        self.vx = 0
+        keys = pg.key.get_pressed()
+        if keys[pg.K_LEFT]:
+            self.vx = -5
+        if keys[pg.K_RIGHT]:
+            self.vx = 5
+        if keys[pg.K_UP]:
+            self.vy = -5
+        if keys[pg.K_DOWN]:
+            self.vy = 5
+
+        self.rect.x += self.vx
+        self.rect.y += self.vy
+
+class Player2(Sprite):
+    def __init__(self):
+        Sprite.__init__(self)
+        self.image = pg.Surface((80,100))
+        self.image.fill(BLACK)
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH / 2, HEIGHT /2)
+        self.vx = 0
+        self.vy = 0
+    def update(self):
+        self.vx = 0
+        keys = pg.key.get_pressed()
+        if keys[pg.K_LEFT]:
+            self.vx = -5
+        if keys[pg.K_RIGHT]:
+            self.vx = 5
+        if keys[pg.K_UP]:
+            self.vy = -5
+        if keys[pg.K_DOWN]:
+            self.vy = 5
+
+        self.rect.x += self.vx
+        self.rect.y += self.vy
